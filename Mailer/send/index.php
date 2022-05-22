135a87927c3c12aba44939306b9394562b586b62
edyMartin2 <edgar.edgarroman@gmail.com>
2022-05-21 22:14:25 -0500

    formulario contactanos

--- Mailer/send/index.php
+++ Mailer/send/index.php
@@ -1,13 +1,51 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-  
+
 require '../../vendor/autoload.php';
-  
+
 $mail = new PHPMailer(true);
 
 
-try{
+@$callback = $_GET['callback'];
+function fillPost($keys, $exclude = null)
+{
+    $array = array();
+    foreach ($_REQUEST as $key => $val) {
+        if (is_array($keys)) {
+            if (in_array($key, $keys)) {
+                $array[$key] = $val;
+            }
+        } elseif ($keys == "ALL") {
+            if (isset($exclude)) {
+                if (is_array($exclude)) {
+                    if (!in_array($key, $exclude)) {
+                        $array[$key] = $val;
+                    }
+                } else {
+                    if ($key != $exclude) {
+                        $array[$key] = $val;
+                    }
+                }
+            } else {
+                $array[$key] = $val;
+            }
+        } else {
+            return $_REQUEST[$keys];
+        }
+    }
+    return $array;
+}
+
+@$variables = fillPost('ALL');
+
+@$Nombre = $variables['Name'];
+@$Correo = $variables['Mail'];
+@$Subject = $variables['Subject'];
+@$Mensaje = $variables['Message'];
+
+try {
     $mail->SMTPDebug = 2;                   // Enable verbose debug output
     $mail->isSMTP();                        // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';    // Specify main SMTP server
@@ -18,15 +56,14 @@ try{
     $mail->Port       = 587;                // TCP port to connect to
 
     $mail->setFrom('from@gfg.com', 'Name');           // Set sender of the mail
-    
-    $mail->addAddress('arcangel580t@gmail.com');           // Add a recipient
-    $mail->addAddress('arcangel580t@gmail.com', 'Angel');   // Name is optional
-    $mail->isHTML(true);                                  
-    $mail->Subject = 'Subject';
-    $mail->Body    = 'HTML message body in <b>bold</b> ';
+
+    $mail->addAddress('edgar.edgarroman@gmail.com');           // Add a recipient
+    $mail->addAddress('edgar.edgarroman@gmail.com', 'Edy');   // Name is optional
+    $mail->isHTML(true);
+    $mail->Subject = $Subject;
+    $mail->Body    =  "Nuevo mensage de {$Nombre} <br> con correo {$Correo} : {$Mensaje}";
     $mail->AltBody = 'Body in plain text for non-HTML mail clients';
     $mail->send();
-
-} catch(Exeption $e){
-    echo($e);
+} catch (Exception $e) {
+    echo  $e;
 }
